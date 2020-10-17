52464f6334719a97d6b21d760e398f4d6fa98b4a
st3phy831 <st3phy831@gmail.com>
2020-10-16 17:17:29 -0700

    Defined Data Objects and included Query

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     //Initializes Parse SDK to as soon as application is created
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("acvmnxEttTimMaGEWG8AKrKppE87Uq7f8UVqahTR")
