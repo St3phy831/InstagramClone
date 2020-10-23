6eddbc4a1ffd7b552b9bb359515ce18e92864d16
st3phy831 <st3phy831@gmail.com>
2020-10-23 11:06:49 -0700

    Updated ParseQuery and created Profile Fragment class

--- app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
@@ -29,8 +29,8 @@ import java.util.List;
 public class PostsFragment extends Fragment {
     public static final String TAG = "PostsFragments";
     private RecyclerView rvPosts;
-    private PostsAdapter adapter;
-    private List<Post> allPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
 
 
     public PostsFragment() {
@@ -64,10 +64,12 @@ public class PostsFragment extends Fragment {
 
     }
 
-    private void queryPosts() {
+    protected void queryPosts() {
         // Specify which class to query
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
